9b5dcead4ff7bdc92a38fd8fa21c52cbd73005b6
akkaassh99 <45987349+akkaassh99@users.noreply.github.com>
2020-08-19 00:40:02 +0530

    Implemented Basic Calculator Service

---
+++ src/main/java/com/akash/BasicCalculatorService.java
@@ -0,0 +1,21 @@
+package com.akash;
+
+public class BasicCalculatorService {
+
+    public int doSum(int i,int j){
+        return i+j;
+    }
+
+    public int doDiff(int i,int j){
+        return i-j;
+    }
+
+    public int multiply(int i,int j){
+        return i*j;
+    }
+
+    public int divide(int i,int j)
+    {
+        return i/j;
+    }
+}
